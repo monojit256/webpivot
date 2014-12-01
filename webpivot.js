669af59d8cb49204cc88ebbb5e9a2f9236d99f8e
monojithalder <monojithalder@hotmail.com>
2014-12-01 15:50:54 +0530

    initial commit

---
+++ webpivot.js
@@ -0,0 +1,26 @@
+//All Custom Javascript Function for web pivot table here..
+
+/**
+ * Implements get_value_from_table for get data from views table and convert into json
+ * @param {Object} th
+ */
+function get_value_from_table(th) {
+	var arr;
+	var i=0;
+	var j=0;
+	alert('asads');
+	//$('tr').each(function() {
+     //arr1[i] = new Array();
+      //arr1[i][0] = $(this).find('td:eq(0)').text();
+      //arr1[i][1] = $(this).find('td:eq(1)').text();
+      //i++;
+    //});
+	alert(i);
+	/*for(j=0; j<=i; j++)
+	{
+		alert(arr[i]);
+	}*/
+}
+$( document ).ready(function() {
+    alert('ssssss');
+});
